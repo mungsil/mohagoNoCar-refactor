c7b5f0126ed3a8ee3efb1b27f50dcffa8c489a76
mungsil <thddmd2009@gmail.com>
2025-02-24 17:29:36 +0900

    refactor: 컨트롤러 응답 DTO의 Wrapper DTO class 생성 및 적용 #36

--- src/main/java/com/example/mohago_nocar/plan/presentation/response/PlanTravelCourseResponseDto.java
+++ src/main/java/com/example/mohago_nocar/plan/presentation/response/PlanTravelCourseResponseDto.java
@@ -1,49 +1,18 @@
 package com.example.mohago_nocar.plan.presentation.response;
 
-import com.example.mohago_nocar.global.common.domain.vo.Location;
-import com.example.mohago_nocar.transit.domain.model.TransitRoute;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record PlanTravelCourseResponseDto(
-        String startPlaceName,
-        double startLongitude,
-        double startLatitude,
-        String endPlaceName,
-        double endLongitude,
-        double endLatitude,
-        int totalTime,
-        double totalDistance,
-        List<SubPathResponseDto> subPaths
+        List<TravelRouteResponseDto> travelRoutes
 ) {
 
-    public static PlanTravelCourseResponseDto of(
-            Location fromLocation,
-            String fromName,
-            Location toLocation,
-            String toName,
-            TransitRoute transitRoute
-    ) {
+    public static PlanTravelCourseResponseDto of(List<TravelRouteResponseDto> travelRoutes) {
         return PlanTravelCourseResponseDto.builder()
-                .startPlaceName(fromName)
-                .startLongitude(fromLocation.getLongitude())
-                .startLatitude(fromLocation.getLatitude())
-                .endPlaceName(toName)
-                .endLongitude(toLocation.getLongitude())
-                .endLatitude(toLocation.getLatitude())
-                .totalTime(transitRoute.getTotalTime())
-                .totalDistance(transitRoute.getTotalDistance())
-                .subPaths(transitRoute.getSubPaths().stream()
-                        .map(subPath ->
-                            switch (subPath.getPathType()) {
-                                case BUS -> BusPathResponseDto.of(subPath);
-                                case WALK -> WalkPathResponseDto.of(subPath);
-                                case SUBWAY -> SubwayPathResponseDto.of(subPath);
-                            })
-                        .toList()
-                )
+                .travelRoutes(travelRoutes)
                 .build();
     }
+
 }
