c1b2b5bf1922a79df64a3f2b07e1627be917a488
mungsil <thddmd2009@gmail.com>
2025-11-01 00:11:43 +0900

    refactor: 패키지 위치 이동

--- src/main/java/com/example/mohago_nocar/plan/domain/service/TravelCourseUseCaseV2.java
+++ src/main/java/com/example/mohago_nocar/plan/domain/service/TravelCourseUseCaseV2.java
@@ -1,7 +1,7 @@
 package com.example.mohago_nocar.plan.domain.service;
 
-import com.example.mohago_nocar.plan.presentation.TravelPlanResponseDtoV2;
-import com.example.mohago_nocar.plan.presentation.request.PlanTravelCourseRequestDtoV2;
+import com.example.mohago_nocar.plan.application.v2.PlanTravelCourseResponseDtoV2;
+import com.example.mohago_nocar.plan.presentation.v2.PlanTravelCourseRequestDtoV2;
 
 public interface TravelCourseUseCaseV2 {
 
